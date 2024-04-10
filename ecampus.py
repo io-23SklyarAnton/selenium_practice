f34901a6fe60d875a41c3862cd74186bc6c6160e
io-23SklyarAnton <80kap.i.toshka@gmail.com>
2024-04-10 14:01:01 +0300

    changed _parse_grade_table() function, optimized XPATHs, its more readable now
    _parse_grade_table() -> _show_grades_tables() Now, this function opens every discipline in the new window, so it provides a better performance. Also, it scans all the tables, it's not depending on the indexes in XPATH anymore, so it is way more stable

--- ecampus.py
+++ ecampus.py
@@ -1,3 +1,4 @@
+import time
 from pprint import pprint
 from typing import Tuple
 import pickle
@@ -13,31 +14,30 @@ from ecampus_config import options_configuration
 load_dotenv()
 
 
-def _parse_grade_table(browser: WebDriver, table_num: int) -> Tuple[str, list]:
-    """return a discipline name and a list of the table rows with grades of this discipline"""
-    discipline = browser.find_element(
+def _show_grades_tables(browser: WebDriver) -> None:
+    """print a disciplines names and a list of the table rows with grades of the all student disciplines"""
+    disciplines = browser.find_elements(
         By.XPATH,
-        f'/html/body/table/tbody/tr[2]/td/table/tbody/tr/td[2]/table/tbody/tr/td/div[1]/div/table/tbody/tr[{table_num}]/td[1]/a'
+        f'//div[@class="studySheetBox"]/table/tbody/tr/td[1]/a'
     )
-    discipline_name = discipline.text
-    discipline.click()
-    rows = browser.find_elements(By.CSS_SELECTOR, '#cMonitoringRow tbody tr')
-    return discipline_name, [row.text for row in rows]
+    for discipline in disciplines:
+        discipline_name = discipline.text
+        browser.execute_script("window.open(arguments[0], '_blank');", discipline.get_attribute("href"))
+        browser.switch_to.window(browser.window_handles[-1])
+
+        rows = browser.find_elements(By.CSS_SELECTOR, '#cMonitoringRow tbody tr')
+        rows_text = [[row.text for row in rows]]
+        print(discipline_name)
+        pprint(rows_text)
+        browser.close()
+        browser.switch_to.window(browser.window_handles[0])
 
 
 def _campus_authentication(browser: WebDriver) -> None:
     # authentication
-    browser.find_element(
-        By.XPATH,
-        '//*[@id="root"]/div/div/div/section/div[1]/div[2]/div/div/div/div/form/fieldset/div[1]/input') \
-        .send_keys(os.getenv("LOGIN"))
-    browser.find_element(
-        By.XPATH,
-        '//*[@id="root"]/div/div/div/section/div[1]/div[2]/div/div/div/div/form/fieldset/div[2]/input') \
-        .send_keys(os.getenv("PASSWORD"))
-    browser.find_element(
-        By.XPATH,
-        '//*[@id="root"]/div/div/div/section/div[1]/div[2]/div/div/div/div/form/fieldset/div[3]/input').click()
+    browser.find_element(By.XPATH, '//input[@placeholder="Логін"]').send_keys(os.getenv("LOGIN"))
+    browser.find_element(By.XPATH, '//input[@placeholder="Пароль"]').send_keys(os.getenv("PASSWORD"))
+    browser.find_element(By.XPATH, '//input[@value="Вхід"]').click()
 
 
 def _save_or_using_cookie_to_auth(browser: WebDriver) -> None:
@@ -52,6 +52,7 @@ def _save_or_using_cookie_to_auth(browser: WebDriver) -> None:
         browser.get('https://ecampus.kpi.ua/home')
     else:
         _campus_authentication(browser)
+        time.sleep(2)
         with open(cookies_path, "wb") as f:
             pickle.dump(browser.get_cookies(), f)
 
@@ -77,12 +78,7 @@ def show_student_grades() -> None:
         # path to the grades tables
         _path_to_the_grades_tables(browser)
 
-        for table_num in range(23, 30):
-            grades_table = _parse_grade_table(browser, table_num)
-            print(grades_table[0])
-            pprint(grades_table[1])
-            # return back to the all tables
-            browser.find_element(By.LINK_TEXT, 'Поточний контроль').click()
+        _show_grades_tables(browser)
 
     except Exception as ex:
         print(ex)
